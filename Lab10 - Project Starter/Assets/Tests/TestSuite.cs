49555fb203bc9e11d8425791e6f833863502c84f
34-haziqdaraqutni <daraqutni92@gmail.com>
2021-10-21 16:15:15 +0800

    update the TestSuite

---
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class TestSuite : MonoBehaviour
+{
+    public float result = 0.0f;
+    [Test]
+    public void TestAddition()
+    {
+        result = Calculator.CalculatePair(5, 2, "+");
+        Assert.Equals(result, 7);
+    }
+    [UnityTest]
+    public IEnumerator TestUnityAddition()
+    {
+        yield return null;
+        result = Calculator.CalculatePair(5, 2, "+");
+        Assert.Equals(result, 7);
+    }
+
+}
